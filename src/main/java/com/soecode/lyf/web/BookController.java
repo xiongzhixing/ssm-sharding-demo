0942f3d3d9f82a5e82fbdb36458a6360e6718b70
xzx <1137112517@qq.com>
2019-12-10 22:22:15 +0800

    'fix'

--- src/main/java/com/soecode/lyf/web/BookController.java
+++ src/main/java/com/soecode/lyf/web/BookController.java
@@ -1,26 +1,17 @@
 package com.soecode.lyf.web;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.Param;
+import com.soecode.lyf.dto.AppointExecution;
+import com.soecode.lyf.dto.Result;
+import com.soecode.lyf.enums.AppointStateEnum;
+import com.soecode.lyf.exception.NoNumberException;
+import com.soecode.lyf.exception.RepeatAppointException;
+import com.soecode.lyf.service.BookService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.soecode.lyf.dto.AppointExecution;
-import com.soecode.lyf.dto.Result;
-import com.soecode.lyf.entity.Book;
-import com.soecode.lyf.enums.AppointStateEnum;
-import com.soecode.lyf.exception.NoNumberException;
-import com.soecode.lyf.exception.RepeatAppointException;
-import com.soecode.lyf.service.BookService;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/book") // url:/模块/资源/{id}/细分 /seckill/list
